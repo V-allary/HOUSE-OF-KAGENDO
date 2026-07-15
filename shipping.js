6b1ddc77561023cf2bfa198619ee9aae4b78ccbf
mac <mac@macs-MacBook-Pro.local>
2026-07-15 06:56:48 -0700

    Update files

---
+++ shipping.js
@@ -0,0 +1,415 @@
+const shippingZones = [
+
+    {
+        zone: "Nairobi CBD",
+        price: 100,
+        keywords: [
+            "cbd",
+            "city centre",
+            "city center",
+            "kenyatta avenue",
+            "kimathi street",
+            "moi avenue",
+            "tom mboya",
+            "biashara street",
+            "standard street",
+            "parliament road",
+            "haile selassie",
+            "koinange street"
+        ]
+    },
+    
+    {
+        zone: "Inner Nairobi Zone",
+        price: 300,
+        keywords: [
+            "parklands",
+            "chiromo",
+            "westlands",
+            "kilimani",
+            "kileleshwa",
+            "lavington",
+            "hurlingham",
+            "state house",
+            "upper hill",
+            "ngara"
+        ]
+    },
+    
+    {
+        zone: "Upper Kiambu Road Area",
+        price: 300,
+        keywords: [
+            "kiambu road",
+            "oilibya plaza",
+            "dci",
+            "aar",
+            "muthaiga mini market"
+        ]
+    },
+    
+    {
+        zone: "Outside Nairobi Parcel Services",
+        price: 350,
+        keywords: [
+            "parcel",
+            "countrywide",
+            "outside nairobi"
+        ]
+    },
+    
+    {
+        zone: "Upper Jogoo Road Area",
+        price: 350,
+        keywords: [
+            "gikomba",
+            "eastleigh",
+            "pangani",
+            "kamkunji"
+        ]
+    },
+    
+    {
+        zone: "Upper Langata Road Area",
+        price: 350,
+        keywords: [
+            "nyayo stadium",
+            "highway mall",
+            "madaraka",
+            "ole sangale",
+            "strathmore",
+            "raila odinga way",
+            "keri road",
+            "ngumo"
+        ]
+    },
+    
+    {
+        zone: "Upper Thika Road Area",
+        price: 400,
+        keywords: [
+            "utalii",
+            "allsops",
+            "roasters",
+            "ngumba",
+            "garden city"
+        ]
+    },
+    
+    {
+        zone: "Upper Mombasa Road Area",
+        price: 400,
+        keywords: [
+            "bunyala road",
+            "lusaka road",
+            "south b",
+            "south c",
+            "nextgen",
+            "nairobi west"
+        ]
+    },
+    
+    {
+        zone: "Upper Ngong Road Area",
+        price: 400,
+        keywords: [
+            "jamhuri",
+            "kibera",
+            "woodley",
+            "prestige",
+            "riara",
+            "mbagathi"
+        ]
+    },
+    
+    {
+        zone: "Upper Waiyaki Way Area",
+        price: 400,
+        keywords: [
+            "abc place",
+            "spring valley",
+            "peponi road",
+            "james gichuru"
+        ]
+    },
+
+    {
+        zone: "Mid Jogoo Road Area",
+        price: 500,
+        keywords: [
+            "makadara",
+            "buruburu",
+            "buru buru",
+            "donholm",
+            "umoja",
+            "komarock",
+            "kayole"
+        ]
+    },
+    
+    {
+        zone: "Mid Kiambu Road Area",
+        price: 500,
+        keywords: [
+            "ridgeways",
+            "thindigua"
+        ]
+    },
+    
+    {
+        zone: "Mid Langata Road Area",
+        price: 500,
+        keywords: [
+            "t mall",
+            "t-mall",
+            "langata",
+            "nairobi dam",
+            "wilson airport",
+            "wilson"
+        ]
+    },
+    
+    {
+        zone: "Mid Ngong Road Area",
+        price: 500,
+        keywords: [
+            "ngong racecourse",
+            "talanta",
+            "talanta sports centre",
+            "talanta sports center"
+        ]
+    },
+    
+    {
+        zone: "Mid Thika Road Area",
+        price: 500,
+        keywords: [
+            "garden estate",
+            "thome",
+            "roysambu",
+            "mirema",
+            "kasarani",
+            "lucky summer",
+            "baba dogo"
+        ]
+    },
+    
+    {
+        zone: "Upper Limuru Road Area",
+        price: 500,
+        keywords: [
+            "muthaiga",
+            "gigiri",
+            "un",
+            "un headquarters",
+            "village market",
+            "rosslyn",
+            "ruaka",
+            "gachie"
+        ]
+    },
+    
+    {
+        zone: "Further Kiambu Road Area",
+        price: 600,
+        keywords: [
+            "runda",
+            "paradise lost",
+            "eden ville",
+            "windsor"
+        ]
+    },
+    
+    {
+        zone: "Inner Ngong Road Area",
+        price: 600,
+        keywords: [
+            "riruta",
+            "karandini",
+            "wanyee",
+            "kawangware",
+            "kabiria",
+            "galleria",
+            "lenana"
+        ]
+    },
+    
+    {
+        zone: "Mid Mombasa Road Area",
+        price: 600,
+        keywords: [
+            "emara",
+            "emara ole sereni",
+            "imara daima",
+            "industrial area"
+        ]
+    },
+    
+    {
+        zone: "Mid Waiyaki Way Area",
+        price: 600,
+        keywords: [
+            "loresho",
+            "mountain view",
+            "kangemi"
+        ]
+    },
+    
+    {
+        zone: "Upper Thika Road Area",
+        price: 600,
+        keywords: [
+            "zimmerman",
+            "githurai 44",
+            "githurai 45",
+            "kahawa",
+            "kahawa wendani",
+            "sukari"
+        ]
+    },
+    {
+        zone: "Further Limuru Road Area",
+        price: 700,
+        keywords: [
+            "ndenderu",
+            "banana",
+            "mucatha",
+            "gathaga",
+            "gacharage"
+        ]
+    },
+    
+    {
+        zone: "Further Ngong Road Area",
+        price: 700,
+        keywords: [
+            "dagoretti",
+            "karen",
+            "bomas",
+            "waithaka"
+        ]
+    },
+    
+    {
+        zone: "Redhill Link Road Area",
+        price: 700,
+        keywords: [
+            "nyari",
+            "kitisuru",
+            "kabete",
+            "thigiri",
+            "thigiri ridge"
+        ]
+    },
+    
+    {
+        zone: "Further Waiyaki Way Area",
+        price: 800,
+        keywords: [
+            "uthiru",
+            "kinoo",
+            "kikuyu",
+            "wangige",
+            "gitaru"
+        ]
+    },
+    
+    {
+        zone: "Jacaranda Gardens Area",
+        price: 800,
+        keywords: [
+            "jacaranda",
+            "kiamumbi",
+            "kahawa west",
+            "mwihoko"
+        ]
+    },
+    
+    {
+        zone: "End of Kiambu Road Area",
+        price: 1000,
+        keywords: [
+            "kist",
+            "muthithi",
+            "njathaini",
+            "kiambu town"
+        ]
+    },
+    
+    {
+        zone: "Further Mombasa Road Area",
+        price: 1000,
+        keywords: [
+            "embakasi",
+            "syokimau",
+            "mlolongo"
+        ]
+    },
+    
+    {
+        zone: "Furthest Jogoo Road Area",
+        price: 1000,
+        keywords: [
+            "nyayo estate",
+            "tasia",
+            "fedha",
+            "airport north road",
+            "saika",
+            "mowlem"
+        ]
+    },
+    
+    {
+        zone: "Rongai Area",
+        price: 1000,
+        keywords: [
+            "rongai",
+            "ongata rongai"
+        ]
+    },
+    
+    {
+        zone: "End of Waiyaki Way Area",
+        price: 1500,
+        keywords: [
+            "zambezi",
+            "sigona",
+            "rironi"
+        ]
+    },
+    
+    {
+        zone: "Juba, South Sudan",
+        price: 1500,
+        keywords: [
+            "juba",
+            "south sudan",
+            "besura logistics"
+        ],
+        note: "Shipping is charged from KES 1,500 per kg. Final cost is confirmed before dispatch."
+    },
+    
+    {
+        zone: "Outskirts Area",
+        price: 1500,
+        keywords: [
+            "athi river",
+            "utawala",
+            "ngong town",
+            "kitengela",
+            "kiserian"
+        ]
+    },
+    
+    {
+        zone: "Ruiru / Membley / Juja / Thika / Tatu City Area",
+        price: 1500,
+        keywords: [
+            "ruiru",
+            "membley",
+            "juja",
+            "thika",
+            "tatu city"
+        ]
+    }
+    ];
